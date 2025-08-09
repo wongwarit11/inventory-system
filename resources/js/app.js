9f02db1a4e106938d3dcd925431c9e0df94d8b77
wongwarit11 <sumitrsirika169@gmail.com>
2025-08-09 13:14:41 +0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
+// resources/js/app.js
+
 import './bootstrap';
+import { app, analytics } from './firebase-config'; // Import the firebase app and analytics instances
+
+// You can now use the 'app' and 'analytics' instances to access Firebase services
+// For example:
+// import { getFirestore } from "firebase/firestore";
+// const db = getFirestore(app);
+// ... your other app logic ...
+
+// This is an example to show that the app and analytics instances are available.
+console.log("Firebase App Initialized:", app);
+console.log("Firebase Analytics Initialized:", analytics);
